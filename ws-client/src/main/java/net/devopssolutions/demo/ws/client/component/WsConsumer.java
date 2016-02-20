37c64b417b71cfca4a7d9233b1ae0f1a59b767f7
Kamil Kiewisz <kamkie@outlook.com>
2016-02-20 02:55:28 +0100

    ping pong from server and client

--- ws-client/src/main/java/net/devopssolutions/demo/ws/client/component/WsConsumer.java
+++ ws-client/src/main/java/net/devopssolutions/demo/ws/client/component/WsConsumer.java
@@ -4,33 +4,58 @@ import lombok.extern.slf4j.Slf4j;
 import org.glassfish.tyrus.client.ClientManager;
 import org.glassfish.tyrus.container.jdk.client.JdkClientContainer;
 import org.glassfish.tyrus.ext.client.java8.SessionBuilder;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import javax.websocket.ClientEndpointConfig;
-import javax.websocket.CloseReason;
-import javax.websocket.EndpointConfig;
-import javax.websocket.Session;
+import javax.websocket.*;
 import java.net.URI;
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
 @Component
 public class WsConsumer {
 
-    private Session session;
+    private final ClientManager clientManager = ClientManager.createClient(JdkClientContainer.class.getName());
+    private final SessionBuilder sessionBuilder = new SessionBuilder(clientManager)
+            .uri(new URI("ws://localhost:8080/ws"))
+            .onOpen(this::onOpen)
+            .messageHandler(PongMessage.class, new MessageHandler.Whole<PongMessage>() {//bug in jdk
+                @Override
+                public void onMessage(PongMessage pongMessage) {
+                    WsConsumer.this.onPong(pongMessage);
+                }
+            })
+            .messageHandler(ByteBuffer.class, this::onMessage)
+            .messageHandler(String.class, this::onMessage)
+            .onError(this::onError)
+            .onClose(this::onClose);
+
+    private final AtomicReference<Session> session = new AtomicReference<>();
+    private final ByteBuffer ping = ByteBuffer.allocate(0);
 
     public WsConsumer() throws Exception {
         log.info("going to open ws connection");
-        ClientEndpointConfig clientEndpointConfig = ClientEndpointConfig.Builder.create()
-                .configurator(new ClientEndpointConfig.Configurator())
-                .build();
-        ClientManager clientManager = ClientManager.createClient(JdkClientContainer.class.getName());
-        this.session = new SessionBuilder(clientManager).uri(new URI("ws://localhost:8080/ws"))
-                .onOpen(this::onOpen)
-                .messageHandler(String.class, this::onMessage)
-                .onError(this::onError)
-                .onClose(this::onClose)
-                .clientEndpointConfig(clientEndpointConfig)
-                .connect();
+        connect();
+    }
+
+    private boolean connect() {
+        try {
+            this.session.set(sessionBuilder.connect());
+            return true;
+        } catch (Exception e) {
+            this.session.set(null);
+            log.warn("couldn't connect", e);
+            return false;
+        }
+    }
+
+    private void onPong(PongMessage pongMessage) {
+        log.info("incoming onPong: {}", pongMessage);
+    }
+
+    private void onMessage(ByteBuffer message) {
+        log.info("new message: {}", message);
     }
 
     private void onMessage(String message) {
@@ -46,6 +71,23 @@ public class WsConsumer {
     }
 
     private void onError(Session session, Throwable throwable) {
-        log.warn("error in ws session: "+ session, throwable);
+        log.warn("error in ws session: " + session, throwable);
     }
+
+    @Scheduled(fixedRate = 10_000)
+    private void sendPing() {
+        Session session = this.session.get();
+        log.info("sending ping: {}", session != null);
+        if (session != null) {
+            try {
+                session.getBasicRemote().sendPing(ping);
+            } catch (Exception e) {
+                this.session.set(null);
+                log.warn("exception sending ping {}", e);
+            }
+        } else {
+            connect();
+        }
+    }
+
 }
--- ws-server/src/main/java/net/devopssolutions/demo/ws/server/handler/WsHandler.java
+++ ws-server/src/main/java/net/devopssolutions/demo/ws/server/handler/WsHandler.java
@@ -1,38 +1,76 @@
 package net.devopssolutions.demo.ws.server.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketMessage;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.*;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Component
 public class WsHandler implements WebSocketHandler {
 
+    private final ConcurrentHashMap<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info("afterConnectionEstablished session: {}", session);
+        sessions.put(session.getId(), session);
     }
 
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-        log.info("handleMessage session: {} message: {}", session, message);
+        log.info("handleMessage session: {}, message: {}", session, message);
     }
 
     @Override
     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
-        log.info("handleTransportError session: " + session, exception);
+        boolean remove = sessions.remove(session.getId(), session);
+        log.info("handleTransportError session: " + session + ", remove: " + remove, exception);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
-        log.info("afterConnectionClosed session: {} closeStatus: {}", session, closeStatus);
+        boolean remove = sessions.remove(session.getId(), session);
+        log.info("afterConnectionClosed session: {}, closeStatus: {}, remove: {}", session, closeStatus, remove);
     }
 
     @Override
     public boolean supportsPartialMessages() {
         return false;
     }
+
+    @Scheduled(fixedRate = 3_000)
+    private void sendHello() {
+        log.info("sending hello: {}", sessions.size());
+        sessions.forEach(10, this::sendHello);
+    }
+
+    private void sendHello(String id, WebSocketSession session) {
+        log.info("sending hello sessionId: {}, session {}", id, session);
+        try {
+            session.sendMessage(new TextMessage("Hello World"));
+        } catch (Exception e) {
+            boolean remove = sessions.remove(id, session);
+            log.warn("exception sending hello, remove session " + remove, e);
+        }
+    }
+
+    @Scheduled(fixedRate = 10_000)
+    private void sendPings() {
+        log.info("sending pings: {}", sessions.size());
+        sessions.forEach(10, this::sendPing);
+    }
+
+    private void sendPing(String id, WebSocketSession session) {
+        log.info("sending ping sessionId: {}, session {}", id, session);
+        try {
+            session.sendMessage(new PingMessage());
+        } catch (Exception e) {
+            boolean remove = sessions.remove(id, session);
+            log.warn("exception sending ping, remove session " + remove, e);
+        }
+    }
+
 }
