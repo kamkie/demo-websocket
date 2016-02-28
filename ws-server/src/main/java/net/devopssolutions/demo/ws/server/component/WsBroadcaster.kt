bb2e6e4adad8c57a7a7794fb105c728ba8624b2f
Kamil Kiewisz <kamkie@outlook.com>
2016-02-29 00:51:00 +0100

    use synchronize on session instead of one queue

--- ws-server/src/main/java/net/devopssolutions/demo/ws/server/component/WsBroadcaster.kt
+++ ws-server/src/main/java/net/devopssolutions/demo/ws/server/component/WsBroadcaster.kt
@@ -3,44 +3,32 @@ package net.devopssolutions.demo.ws.server.component
 import net.devopssolutions.demo.ws.server.util.LoggingThreadPoolExecutor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import java.io.IOException
 import java.nio.ByteBuffer
 import java.security.Principal
 import java.util.concurrent.ArrayBlockingQueue
 import java.util.concurrent.TimeUnit
-import javax.annotation.PostConstruct
 
 @Component
 class WsBroadcaster {
     private val log = org.slf4j.LoggerFactory.getLogger(WsBroadcaster::class.java)
 
     private val sendersExecutor = LoggingThreadPoolExecutor(10, 20, 2, TimeUnit.MINUTES, ArrayBlockingQueue<Runnable>(10000))
-    private val messagesQueue = ArrayBlockingQueue<Pair<String, ByteBuffer>>(100000);
 
     @Autowired
     private lateinit var wsServer: WsServer
-
-    @PostConstruct
-    fun init() {
-        sendersExecutor.execute {
-            while (true) {
-                try {
-                    val task = messagesQueue.take()
-                    wsServer.getSession(task.first)?.apply {
-                        this.basicRemote.sendBinary(task.second)
-                    }
-                } catch (e: Exception) {
-                    log.warn("exception broadcasting message", e)
-                }
-            }
-        }
-    }
 
     fun broadcastAsync(action: () -> ByteBuffer) {
         sendersExecutor.execute {
             val payload = action()
             wsServer.getAllSessions().forEach {
-                val task = it.id.to(payload)
-                messagesQueue.put(task)
+                sendersExecutor.execute {
+                    wsServer.getSession(it.id)?.apply {
+                        synchronized(this) {
+                            this.basicRemote.sendBinary(payload)
+                        }
+                    }
+                }
             }
         }
     }
@@ -49,21 +37,27 @@ class WsBroadcaster {
         wsServer.getAllSessions().forEach {
             sendersExecutor.execute {
                 val payload = action(it.userPrincipal)
-                val task = it.id.to(payload)
-                messagesQueue.put(task)
+                wsServer.getSession(it.id)?.apply {
+                    synchronized(this) {
+                        this.basicRemote.sendBinary(payload)
+                    }
+                }
             }
         }
     }
 
     fun sendToIdAsync(sessionId: String, action: () -> ByteBuffer) {
         if (wsServer.getSession(sessionId) == null) {
-            log.warn("session not exist")
-            return
+            throw IOException("session not exist")
         }
         sendersExecutor.execute {
             val payload = action()
             val task = sessionId.to(payload)
-            messagesQueue.put(task)
+            wsServer.getSession(task.first)?.apply {
+                synchronized(this) {
+                    this.basicRemote.sendBinary(task.second)
+                }
+            }
         }
     }
 }
