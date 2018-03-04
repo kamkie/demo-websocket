8a9891e26c56a136ba58c0472ad1721cb9ec5a3b
Kamil Kiewisz <kamkie@outlook.com>
2018-03-04 17:18:54 +0100

    fixed usage of deprecated code

--- ws-client/src/main/java/net/devopssolutions/demo/ws/client/BootClient.java
+++ ws-client/src/main/java/net/devopssolutions/demo/ws/client/BootClient.java
@@ -1,5 +1,6 @@
 package net.devopssolutions.demo.ws.client;
 
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BootClient {
 
     public static void main(String[] args) throws Exception {
-        new SpringApplicationBuilder(BootClient.class).web(false).run(args);
+        new SpringApplicationBuilder(BootClient.class).web(WebApplicationType.NONE).run(args);
 
         Thread.currentThread().join();
     }
